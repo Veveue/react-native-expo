c270b78ee39741ede7116d2fa36fdbbf9d55fc1d
veveue <veveue@dingtalk.com>
2018-09-20 22:55:56 +0800

    eslint

---
+++ egg/egg-example/app/controller/admin.js
@@ -0,0 +1,16 @@
+const { Controller } = require("egg");
+
+class AdminController extends Controller {
+  async index() {
+    // egg基于koa
+
+    // koa给用户相应信息
+    // ctx.body='用户管理';
+    console.log(this);
+
+    // egg给用户相应信息
+    this.ctx.body = "用户管理";
+  }
+}
+
+module.exports = AdminController;
